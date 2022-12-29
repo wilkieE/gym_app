cb8ec46ec4c8fa7f08d65f408226eb58f92053aa
Ejiroghene Philip Wilkie <48595932+wilkieE@users.noreply.github.com>
2022-12-29 08:16:28 +0200

    Enabling API calls for v1 build

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -13,7 +13,7 @@ export const youtubeOptions = {
   },
 };
 export const fetchData = async (url, options) => {
-  // const response = await fetch(url, options);
-  // const data = await response.json();
+  const response = await fetch(url, options);
+  const data = await response.json();
   return data;
 };
